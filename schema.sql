28cb93cec0f5d8570e5a8495312012c743a0a686
Thomasbrown6 <Thomas.brown1125@gmail.com>
2018-07-10 00:10:56 -0400

    added schema

---
+++ schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE database bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+  item_id INT AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(100) NULL,
+  department_name VARCHAR(100) NULL,
+  price DECIMAL(10,2) NULL,
+  stock_quantity INT(10) NOT NULL,
+  PRIMARY KEY (item_id)
+);
+
+
+
+
